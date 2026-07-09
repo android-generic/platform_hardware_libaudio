a66cc22f4bde8d2a84487cb3ba4e0eea4df340ad
hmtheboy154 <buingoc67@gmail.com>
2026-07-09 04:30:52 -0400

    Massive cleanup & adapt to new type of packages
    - alsa-lib confs & alsactl init are now soong targets that will
    be copied alongside libasound & alsactl building. We can get rid
    of the old copying method now.
    - Build alsa_tplg & the nhlt module.
    - Build Celadon USB audio HAL.
    - Include sof-firmware & alsa-ucm-conf packages as we can be able
    to generate Android.bp for them.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- alsa.mk
+++ alsa.mk
@@ -8,46 +8,18 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 
-TARGET_ALSA_CONF_DIR := system/usr/share/alsa
-LOCAL_ALSA_CONF_DIR  := external/alsa-lib/src/conf
-
-copy_conf := \
-	alsa.conf \
-	cards/aliases.conf \
-	ctl/default.conf \
-	pcm/center_lfe.conf \
-	pcm/default.conf \
-	pcm/dmix.conf \
-	pcm/dpl.conf \
-	pcm/dsnoop.conf \
-	pcm/front.conf \
-	pcm/hdmi.conf \
-	pcm/iec958.conf \
-	pcm/modem.conf \
-	pcm/rear.conf \
-	pcm/side.conf \
-	pcm/surround21.conf \
-	pcm/surround40.conf \
-	pcm/surround41.conf \
-	pcm/surround50.conf \
-	pcm/surround51.conf \
-	pcm/surround71.conf \
-	smixer.conf \
-
-LOCAL_ALSA_INIT_DIR  := external/alsa-utils/alsactl/init
-
-copy_init := \
-	00main \
-	ca0106 \
-	default \
-	hda \
-	help \
-	info \
-	test
+# Prebuilts/Configs/Firmwares
+PRODUCT_PACKAGES += \
+	sof-firmware \
+	alsa-ucm-conf-common \
+	alsa-ucm-conf-amd \
+	alsa-ucm-conf-hda \
+	alsa-ucm-conf-intel \
+	alsa-ucm-conf-samsung \
+	alsa-ucm-conf-sof-soundwire \
+	alsa-ucm-conf-usb-audio
 
 PRODUCT_COPY_FILES := \
-	$(foreach f,$(copy_conf),$(LOCAL_ALSA_CONF_DIR)/$(f):$(TARGET_ALSA_CONF_DIR)/$(f)) \
-	$(foreach f,$(copy_init),$(LOCAL_ALSA_INIT_DIR)/$(f):$(TARGET_ALSA_CONF_DIR)/init/$(f)) \
     $(LOCAL_PATH)/policy/audio_policy_criteria.conf:system/etc/audio_policy_criteria.conf \
     $(LOCAL_PATH)/policy/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/policy/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
@@ -62,16 +34,19 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/mixer_paths_usb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_usb.xml
 
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
 	alsa_mixer \
 	alsa_amixer \
 	alsa_aplay \
 	alsa_ctl \
 	alsa_ucm \
+	alsa_tplg \
 	alsa_iecset \
+	libalsatplg_module_nhlt \
 	audio.primary.x86 \
 	audio.primary.x86_celadon \
 	audio.hdmi.x86 \
 	audio.bluetooth.default \
 	audio.usb.default \
+	audio.usb.x86 \
 	audio.r_submix.default \
